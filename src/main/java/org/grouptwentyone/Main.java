9fb477632f3aac9800693cad425aaccd7ef891e2
YazAtth <yasith.athan@gmail.com>
2023-01-28 10:50:29 +0000

    FEAT: Created StartController class

--- src/main/java/org/grouptwentyone/Main.java
+++ src/main/java/org/grouptwentyone/Main.java
@@ -5,6 +5,6 @@ import org.grouptwentyone.models.RandomNumberGenerator;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(RandomNumberGenerator.getRandomNumberInRange(9, 3));
+        System.out.println(RandomNumberGenerator.getRandomNumberInRange(2, 9));
     }
 }
---
+++ src/main/java/org/grouptwentyone/controllers/StartController.java
@@ -0,0 +1,4 @@
+package org.grouptwentyone.controllers;
+
+public class StartController {
+}
