1d1dfedc63fcaf7ec74b3b2017876e8ffdc0039c
dominicjk <jkdominic752@gmail.com>
2023-02-05 13:02:23 +0000

    FEAT : First attempt of include the displayTiles method

--- src/main/java/org/grouptwentyone/views/TestBoardView.java
+++ src/main/java/org/grouptwentyone/views/TestBoardView.java
@@ -1,6 +1,7 @@
 package org.grouptwentyone.views;
 
 import org.grouptwentyone.controllers.HabitatTilesController;
+import org.grouptwentyone.models.HabitatTile;
 import org.grouptwentyone.models.HexCoordinate;
 import org.grouptwentyone.models.Tile;
 
@@ -26,14 +27,66 @@ public class TestBoardView {
         Tile oneTwo = new Tile(HabitatTilesController.habitatTilesBag.get(1), new HexCoordinate(1, 2));
         Tile twoOne = new Tile(HabitatTilesController.habitatTilesBag.get(2), new HexCoordinate(2, 1));
 
-
         playerBoard.get(1).add(1, oneOne);
         //note oneTwo is added at index [2, 1]
         playerBoard.get(2).add(1, oneTwo);
         playerBoard.get(1).add(2, twoOne);
+
+        //dummy loops //DELETE ONCE ARRAY LISTS ARE SIZE THREE
+        System.out.println(playerBoard.size());
+        System.out.println(playerBoard.get(0).size());
+        System.out.println(playerBoard.get(1).size());
+        System.out.println(playerBoard.get(2).size());
+
+
+        for (int row = 0; row < playerBoard.size() ; row++) {
+            for (int col = 0; col < playerBoard.get(row).size(); col++) {
+                Tile currTile = playerBoard.get(row).get(col);
+                System.out.println(currTile.isActive());
+            }
+        }  //END OF DUMMY LOOPS
+    }
+
+    //static method to display tiles
+    public static String displayTiles(ArrayList<ArrayList<Tile>> playerBoard) {
+        StringBuilder pattern = new StringBuilder();
+
+            //iterate over each ArrayList in the ArrayList (rows)
+        for (int row = 0; row < playerBoard.size(); row++) {
+            //iterate over each row of each tile (tiles size is 6)
+            for (int i = 0; i < 6; i++) {
+
+                if (row % 2 == 0) { //slight offset for even columns
+                    pattern.append("       ");
+                }
+
+                //iterate over each Tile
+                for (int col = 0; col < playerBoard.get(row).size(); col++) {
+
+                    Tile currTile = playerBoard.get(row).get(col);
+                    HabitatTile currHabitatTile = playerBoard.get(row).get(col).getHabitatTile();
+
+
+                    if (!playerBoard.get(row).get(col).isActive()) {
+                        pattern.append("               ");
+                    } else if (i == 0 || i == 5) {
+                        pattern.append("    *  *  *    ");
+                    } else if (i == 1 || i == 4) {
+                        pattern.append("  *         *  ");
+                    } else if (i == 2 || i == 3) {
+                        pattern.append(" *           * ");
+                    } else {
+                        pattern.append("*             *");
+                    }
+                }
+                pattern.append("\n");
+            }
+        }
+        return pattern.toString();
     }
 
     public static void main(String[] args) {
         addToBoard();
+        //System.out.println(displayTiles(playerBoard));
     }
 }
