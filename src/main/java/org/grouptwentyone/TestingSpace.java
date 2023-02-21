1e11b8a047ab19e7922c556f1e7d2a1d02b6ac2b
YazAtth <yasith.athan@gmail.com>
2023-02-21 21:53:01 +0000

    FEAT: Added a class with a main to test methods

---
+++ src/main/java/org/grouptwentyone/TestingSpace.java
@@ -0,0 +1,27 @@
+import org.grouptwentyone.models.HabitatTile;
+import org.grouptwentyone.models.HexCoordinate;
+import org.grouptwentyone.models.Player;
+
+public class TestingSpace {
+    public static void main(String[] args) {
+        Player p1 = new Player("bob");
+        HabitatTile habitatTile = new HabitatTile(2);
+        p1.setSelectedTile(habitatTile);
+        p1.addNewTile(new HexCoordinate(0,0));
+
+//        p1.addPlayerBoardBuffer(Player.PlayerBoardSide.TOP);
+//        p1.addPlayerBoardBuffer(Player.PlayerBoardSide.TOP);
+        p1.addPlayerBoardBuffer(Player.PlayerBoardSide.LEFT);
+        p1.addPlayerBoardBuffer(Player.PlayerBoardSide.BOTTOM);
+        p1.addPlayerBoardBuffer(Player.PlayerBoardSide.RIGHT);
+
+
+//        System.out.println(p1.getPlayerBoard().get(0));
+
+//        System.out.println(p1.isCoordinateOnBoardEdge(new HexCoordinate(0, 4)));
+//        System.out.println(p1.getPartOfBoardCoordinateIsOn(new HexCoordinate(4,5)));
+//        System.out.println(p1.getPlayerBoard().get(3).get(4));
+//        System.out.println("\n");
+        p1.printPlayerBoard();
+    }
+}
