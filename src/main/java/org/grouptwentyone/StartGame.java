9b1d20638e9cbe98784f9f769daacd2fa9df417d
dominicjk <jkdominic752@gmail.com>
2023-03-27 00:01:06 +0100

    FEAT: playerBoardInstruction class added

--- src/main/java/org/grouptwentyone/StartGame.java
+++ src/main/java/org/grouptwentyone/StartGame.java
@@ -42,6 +42,7 @@ public class StartGame {
 
         GameUiView.printPageBorder();
         GameUiView.printScoreCardRules();
+        PlayerBoardInstructions.displayBoardInstructions();
         GameSetupView.displayPlayerOrder(playerList);
 
         //remove habitat tiles depending on number of players
---
+++ src/main/java/org/grouptwentyone/views/PlayerBoardInstructions.java
@@ -0,0 +1,36 @@
+package org.grouptwentyone.views;
+
+public class PlayerBoardInstructions {
+    String r = GameUiView.RED;
+    String g = GameUiView.GREEN_BOLD;
+    String y = GameUiView.YELLOW_BOLD_BRIGHT;
+    public static void displayBoardInstructions() {
+        System.out.println("Each tile will have the possibility of being connect to at most 6 other tiles");
+        System.out.println("A tile is adjacent to another tile if and only their corners touch\n" +
+                            "Below is sample board which marks all the adjacent tiles of position (2, 1) in green ");
+        System.out.println(  GameUiView.RED +   "           *  *  *    "  + GameUiView.GREEN +  "   *  *  *        *  *  *                   \n" +
+                             GameUiView.RED +   "         *         *  "  + GameUiView.GREEN+   " *         *    *         *                 \n" +
+                             GameUiView.RED +   "        *           * "  + GameUiView.GREEN+   "*           *  *           *                \n" +
+                             GameUiView.RED +   "        *   (0,0)   * "  + GameUiView.GREEN+   "*   (0,1)   *  *   (0,2)   *                \n" +
+                             GameUiView.RED +   "         *         *  "  + GameUiView.GREEN+   " *         *    *         *                 \n" +
+                             GameUiView.RED +   "           *  *  *      "  + GameUiView.GREEN+ "   *  *  *        *  *  *                   \n" +
+                                                "                   * * * *    "  + GameUiView.YELLOW_BOLD_BRIGHT+ "    * * * * "   + GameUiView.GREEN+ "     *  *  *    \n" +
+                                                "                 *         *  "  + GameUiView.YELLOW_BOLD_BRIGHT+ "  *         * " + GameUiView.GREEN+ "   *         *  \n" +
+                                                "                *           * "  + GameUiView.YELLOW_BOLD_BRIGHT+ " *           * " + GameUiView.GREEN+ " *           * \n" +
+                                                "                *   (1,1)   * "  + GameUiView.YELLOW_BOLD_BRIGHT+ " *   (1,2)   * " + GameUiView.GREEN+ " *   (1,3)   * \n" +
+                                                "                 *         *  "  + GameUiView.YELLOW_BOLD_BRIGHT+ "  *         *  " + GameUiView.GREEN+ "  *         *  \n" +
+                                                "                   * * * *    "  + GameUiView.YELLOW_BOLD_BRIGHT+ "    * * * *       " + GameUiView.GREEN+ " *  *  *    \n" +
+                            GameUiView.RED +    "           *  *  *    "  + GameUiView.GREEN +  "    *  *  *                   \n" +
+                            GameUiView.RED +    "         *         *  "  + GameUiView.GREEN +  "  *         *                 \n" +
+                            GameUiView.RED +    "        *           * "  + GameUiView.GREEN +  " *           *                \n" +
+                            GameUiView.RED +    "        *   (2,0)   * "  + GameUiView.GREEN +  " *   (2,2)   *                \n" +
+                            GameUiView.RED +    "         *         *  "  + GameUiView.GREEN +  "  *         *                 \n" +
+                            GameUiView.RED +    "           *  *  *    "  + GameUiView.GREEN +  "    *  *  *                   \n" +
+                            GameUiView.RED +    "                   *  *  *        *  *  *                   \n" +
+                                                "                 *         *    *         *                 \n" +
+                                                "                *           *  *           *                \n" +
+                                                "                *   (3,1)   *  *   (3,2)   *                \n" +
+                                                "                 *         *    *         *                 \n" +
+                                                "                   *  *  *        *  *  *         ");
+    }
+}
