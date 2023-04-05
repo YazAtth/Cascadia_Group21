76686d4cedd97f8274fcbc3521a4bbe0d715a719
YazAtth <yasith.athan@gmail.com>
2023-04-05 16:19:19 +0100

    FEAT: Added CustomPair class

---
+++ src/main/java/org/grouptwentyone/models/CustomPair.java
@@ -0,0 +1,32 @@
+package org.grouptwentyone.models;
+
+public class CustomPair<S,T> {
+
+    private S field1;
+    private T field2;
+
+    public CustomPair(S field1, T field2) {
+        this.field1 = field1;
+        this.field2 = field2;
+    }
+
+    public S getField1() {
+        return field1;
+    }
+
+    public void setField1(S field1) {
+        this.field1 = field1;
+    }
+
+    public T getField2() {
+        return field2;
+    }
+
+    public void setField2(T field2) {
+        this.field2 = field2;
+    }
+
+    public String toString() {
+        return String.format("('%s', '%s')", this.getField1(), this.getField2());
+    }
+}
