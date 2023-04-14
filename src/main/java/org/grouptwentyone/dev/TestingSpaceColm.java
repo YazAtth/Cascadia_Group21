490564e0e271d042b82f1c99fb3597454973e1f0
C0hAongha <102315030+C0hAongha@users.noreply.github.com>
2023-04-14 14:58:24 +0100

    TestSpace: implemented code that runs game N num of times

--- src/main/java/org/grouptwentyone/dev/TestingSpaceColm.java
+++ src/main/java/org/grouptwentyone/dev/TestingSpaceColm.java
@@ -2,7 +2,9 @@ package org.grouptwentyone.dev;
 
 import org.grouptwentyone.StartGame;
 import org.grouptwentyone.controllers.HabitatTilesController;
+import org.grouptwentyone.controllers.StarterHabitatTilesController;
 import org.grouptwentyone.controllers.UserTerminationController;
+import org.grouptwentyone.controllers.WildlifeTokensController;
 import org.grouptwentyone.models.CascadiaBot;
 import org.grouptwentyone.models.Player;
 import org.grouptwentyone.models.PlayerManager;
@@ -10,7 +12,13 @@ import org.grouptwentyone.views.*;
 
 import java.util.ArrayList;
 
+import static org.grouptwentyone.controllers.WildlifeTokensController.createBagOfWildlifeTokens;
+
 public class TestingSpaceColm {
+    public static int maxScore = -1;
+    public static String maxName = "";
+    public static int minScore = 1000;
+    public static String minName = "";
     private static void testBotGame() {
         ArrayList<Player> playerList = new ArrayList<>();
         playerList.add(new CascadiaBot("bot1"));
@@ -34,9 +42,9 @@ public class TestingSpaceColm {
             }
 
             //next player
-            System.out.println("Moving to next player");
+            //System.out.println("Moving to next player");
             activePlayer = playerManager.cycleToNextPlayer();
-            GameUiView.printLargeSpace();
+            //GameUiView.printLargeSpace();
         }
 
         System.out.println("No tiles remain so play is finished, calculating player score...");
@@ -46,13 +54,23 @@ public class TestingSpaceColm {
 
         //end program
         //UserTerminationController.endProgram();
-//        GameUiView.printLargeSpace();
+        //GameUiView.printLargeSpace();
     }
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        testBotGame();
+        for (int i = 0; i < 1000; i++) {
+            StartGame.tilesRemain = true;
+            WildlifeTokensController.wildlifeTokenBag = createBagOfWildlifeTokens();
+            HabitatTilesController.habitatTilesBag = HabitatTilesController.createBagOfHabitatTiles();
+            StartGame.selectedTiles = SelectionOptionsView.getFourHabitatTiles();
+            StartGame.selectedTokens = SelectionOptionsView.getFourWildlifeTokens();
+            StarterHabitatTilesController.starterHabitatTilesBag = StarterHabitatTilesController.createBagOfStarterHabitatTiles();
+            testBotGame();
+        }
         long endTime = System.currentTimeMillis();
         System.out.println("Time taken: " + (endTime-startTime));
+        System.out.println("Max Score = " + maxScore + ", name: " + maxName);
+        System.out.println("Min Score = " + minScore + ", name: " + minName);
     }
 }
