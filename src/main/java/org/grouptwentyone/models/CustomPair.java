0def9634a221aabe4f2205eadd4ff863b4f61504
C0hAongha <102315030+C0hAongha@users.noreply.github.com>
2023-05-01 22:57:19 +0100

    Added headers to new files

--- src/main/java/org/grouptwentyone/models/CustomPair.java
+++ src/main/java/org/grouptwentyone/models/CustomPair.java
@@ -1,3 +1,12 @@
+/*
+ * Cascadia
+ * 21: Group 21
+ * Student name:            GitHub ID:
+ * Yasith Atthanayake       YazAtth
+ * Colm Ó hAonghusa         C0hAongha
+ * Dominykas Jakubauskas    dominicjk
+ */
+
 package org.grouptwentyone.models;
 
 public class CustomPair<S,T> {
